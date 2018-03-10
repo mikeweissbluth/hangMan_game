11efb16c890fd1ff0a2b8df39f72eff1cdc82680
Michael Weisssbluth <thelab@Michaels-MacBook-Pro.local>
2018-03-09 22:02:36 -0600

    second commit w better improvements.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,5 @@
+/** variables */
+// array of word options
 let words = [ "git", "commit", "documentobjectmodel", "dorks", "array", "flexbox", "scottawesome"];
 let lettersGuessed = [];
 let numberSpaces = [];
@@ -7,46 +9,51 @@ let wins = 0;
 let randomNumber ;
 let computerChoice ;
 let computerArray ;
+let losses = 0;
 
 function init() {
   numberSpaces = [];
   guessRemain = 10;
-  document.getElementById("guesses-remaining").innerHTML = guessRemain;
+  document.getElementById("guesses-remaining").textContent = guessRemain;
   lettersGuessed = [];
-  document.getElementById("already-guessed").innerHTML = lettersGuessed;
+  document.getElementById("already-guessed").textContent = lettersGuessed;
   randomNumber = Math.floor((Math.random() * 5));
   computerChoice = words[randomNumber]; 
   computerArray = Array.from(computerChoice);
   console.log(computerArray);
   for (var i=0; i < computerArray.length; i++) {
     numberSpaces.push("_");
-    document.getElementById("blanks").innerHTML = numberSpaces.join(" ");
+    document.getElementById("blanks").textContent = numberSpaces.join(" ");
   }
 }
+init ();
+
 document.onkeyup = function(event) { 
   userText = event.key;
     if (computerArray.includes(userText) === true) {
       for (var i=0; i < computerArray.length; i++) {
         if (computerArray[i] === userText) {
         numberSpaces[i] = userText; 
-        document.getElementById("blanks").innerHTML = numberSpaces.join(" ");
+        document.getElementById("blanks").textContent = numberSpaces.join(" ");
         } 
       }
     } else {
       lettersGuessed.push(userText);
       console.log(lettersGuessed);
-      document.getElementById("already-guessed").innerHTML = lettersGuessed;
+      document.getElementById("already-guessed").textContent = lettersGuessed.sort();
       console.log(computerArray.includes(userText));
       guessRemain--;
-      document.getElementById("guesses-remaining").innerHTML = guessRemain;
+      document.getElementById("guesses-remaining").textContent = guessRemain;
     }
   if (guessRemain === 0 ) {
     // alert("you lose");
+    losses++; 
+    document.getElementById("losses").textContent = losses;
     init();
   }
   if (computerArray.toString() == numberSpaces.toString()) {
     wins++;
-    document.getElementById("win-win").innerHTML = wins;
+    document.getElementById("win-win").textContent = wins;
     init();
   } 
 
